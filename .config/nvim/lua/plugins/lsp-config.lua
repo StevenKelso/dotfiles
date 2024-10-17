c2527ce2a2cf05c53e9d6ff8762813c189048935
StevenKelso <the.steven.kelso@gmail.com>
2024-10-17 10:26:10 -0400

    tweaking colors

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "pylsp" }
+                ensure_installed = { "lua_ls", "ts_ls", "pylsp", "bashls" }
             })
         end
     },
@@ -20,6 +20,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
             lspconfig.pylsp.setup({})
+            lspconfig.bashls.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
