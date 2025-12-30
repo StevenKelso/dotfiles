8245c4297ded1c4ced35f8e4a99abcf51b556287
stevenkelso <the.steven.kelso@gmail.com>
2025-12-30 08:16:05 -0500

    hyprpland breaking changes config fix

--- .config/nvim/lua/plugins/mini-pick.lua
+++ .config/nvim/lua/plugins/mini-pick.lua
@@ -3,6 +3,6 @@ return {
         'nvim-mini/mini.pick',
         opts = {},
         vim.keymap.set('n', '<Leader>ff', function() MiniPick.builtin.files({ tool = 'git'}) end, { desc = 'Find Files' }),
-        vim.keymap.set('n', '<Leader>fg', function() MiniPick.builtin.grep_live() end, { desc = 'Grep (search in files)' }),
+        vim.keymap.set('n', '<Leader>fg', function() MiniPick.builtin.grep_live({ tool = 'git'}) end, { desc = 'Grep (search in files)' }),
     }
 }
